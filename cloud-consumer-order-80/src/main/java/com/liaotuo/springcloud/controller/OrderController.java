59f02b420208a324dc659bf58c416409ff02d988
lt <lt@youchai.me>
2020-03-13 14:26:00 +0800

    新增 消费者

---
+++ cloud-consumer-order-80/src/main/java/com/liaotuo/springcloud/controller/OrderController.java
@@ -0,0 +1,30 @@
+package com.liaotuo.springcloud.controller;
+
+import com.liaotuo.springcloud.entities.CommonResult;
+import com.liaotuo.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@Slf4j
+@RestController
+public class OrderController {
+    public static final String PAYMENT_URL = "http://localhost:8001";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @PostMapping(value = "/consumer/payment/create")
+    @ResponseBody
+    public CommonResult create(Payment payment) {
+        return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+    }
+
+    @GetMapping(value = "/consumer/payment/get/{id}")
+    @ResponseBody
+    public CommonResult getPaymentById(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+}
